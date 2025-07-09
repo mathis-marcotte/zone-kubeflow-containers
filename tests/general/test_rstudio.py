2785d4045e9ed56575d28b404d145dfc3ce3f15e
anrayliu <anrayliu@gmail.com>
2025-07-09 19:21:36 +0000

    feat: update rstudio
    Update rstudio to latest stable 2025.05.1-513 and update test

--- tests/general/test_rstudio.py
+++ tests/general/test_rstudio.py
@@ -5,7 +5,7 @@ from helpers import CondaPackageHelper
 
 LOGGER = logging.getLogger(__name__)
 
-EXPECTED = "2024.12.0+467 (Kousa Dogwood) for Ubuntu Jammy"
+EXPECTED = "2025.05.1+513 (Mariposa Orchid) for Ubuntu Jammy"
 
 @pytest.fixture(scope="function")
 def package_helper(container):
